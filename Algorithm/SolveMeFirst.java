85529f43c31f892959ff0b049190e76b7405eaa9
meilisady <meilisa.effendy10@gmail.com>
2024-01-24 14:21:23 +0700

    day 1

---
+++ Algorithm/SolveMeFirst.java
@@ -0,0 +1,16 @@
+package Algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class SolveMeFirst {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner STDIN = new Scanner(System.in);
+        int a=STDIN.nextInt();
+        int b=STDIN.nextInt();
+        int output= a+b;
+        System.out.println(output);
+    }
+}
